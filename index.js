7db52c5b254e73bbbaedf9fc6c97437de57702ec
KMitros <offmop1@gmail.com>
2018-12-02 23:54:03 +0300

    another commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var PIXI = require("pixi.js");
-var app = new PIXI.Application(1000,90,{
-    roundPixels:
+let PIXI = require("pixi.js");
+let pS = require('pixi-sound')
+let app = new PIXI.Application(1440,240,{
+    transparent: true
 });
 
+const sound = PIXI.sound.Sound.from('Button_Click.wav');
 
 
 document.querySelector('#menu').appendChild(app.view);
@@ -11,44 +13,55 @@ document.querySelector('#menu').appendChild(app.view);
 
 let container = new PIXI.Container();
 let bets = new PIXI.Container();
+let coins = new PIXI.Container();
 
 
-var background = PIXI.Sprite.fromImage('img/bg_copy.png');
-background.width = 1440;
-background.height = 90;
+
+
+
+let background = PIXI.Sprite.fromImage('img/bg_copy.png');
+
 
 
 
 
 // create some textures from an image path
-var textureButton = PIXI.Texture.fromImage('img/butt-i-n.png');
-var textureButtonDown = PIXI.Texture.fromImage('img/butt-i-down.png');
-var textureButtonOver = PIXI.Texture.fromImage('img/butt-i-hover.png');
+let textureButton = PIXI.Texture.fromImage('img/butt-i-n.png');
+let textureButtonDown = PIXI.Texture.fromImage('img/butt-i-down.png');
+let textureButtonOver = PIXI.Texture.fromImage('img/butt-i-hover.png');
 
-var textureButtonPlus = PIXI.Texture.fromImage('img/butt-plus.png');
-var textureButtonPlusDown = PIXI.Texture.fromImage('img/butt-plus-hover.png');
-var textureButtonPlusOver = PIXI.Texture.fromImage('img/butt-plus-down.png');
+let textureButtonPlus = PIXI.Texture.fromImage('img/butt-plus.png');
+let textureButtonPlusDown = PIXI.Texture.fromImage('img/butt-plus-down.png');
+let textureButtonPlusOver = PIXI.Texture.fromImage('img/butt-plus-hover.png');
 
-var textureBet =  PIXI.Texture.fromImage('img/bet_field.png');
+let textureBet =  PIXI.Texture.fromImage('img/bet_field.png');
 
-var buttons = [];
+let textureCoin = PIXI.Texture.fromImage('img/coin_bg.png');
 
-var buttonPositions = [
-    85, 65
-];
 
-var button = new PIXI.Sprite(textureButton);
-var buttonPlus = new PIXI.Sprite(textureButtonPlus);
-var betbg = new PIXI.Sprite(textureBet);
+let buttons = [];
 
 
 
+let button = new PIXI.Sprite(textureButton);
+let buttonPlus = new PIXI.Sprite(textureButtonPlus);
+let buttonMinus = new PIXI.Sprite(textureButtonPlus);
+let betbg = new PIXI.Sprite(textureBet);
+let coinbg = new PIXI.Sprite(textureCoin);
 
 
 
+buttonPlus.down = textureButtonPlusDown;
+buttonPlus.over = textureButtonPlusOver;
+buttonPlus.normal = textureButtonPlus;
 
+buttonMinus.down = textureButtonPlusDown;
+buttonMinus.over = textureButtonPlusOver;
+buttonMinus.normal = textureButtonPlus;
 
-
+button.down = textureButtonDown;
+button.over = textureButtonOver;
+button.normal = textureButton;
 
 
 
@@ -57,8 +70,7 @@ function constr(butt){
     butt.buttonMode = true;
 
     butt.anchor.set(0.5);
-    butt.x = buttonPositions[0];
-    butt.y = buttonPositions[1];
+
 
     // make the button interactive...
     butt.interactive = true;
@@ -93,32 +105,63 @@ function constr(butt){
     buttons.push(butt);
 }
 
-constr(button);
+function constrToggle(butt){
 
-constr(buttonPlus);
+    butt.buttonMode = true;
 
-// set some silly values...
-buttons[0].scale.set(.5);
+    butt.anchor.set(0.5);
 
 
-buttons[1].x = 50;
-buttons[1].y = 55;
+    // make the button interactive...
+    butt.interactive = true;
+    butt.buttonMode = true;
+
+    butt
+    // Mouse & touch events are normalized into
+    // the pointer* events for handling different
+    // button events.
+        .on('pointerdown', onButtonDown)
+        .on('pointerup', onButtonUp)
+        .on('pointerupoutside', onButtonUp)
+        .on('pointerover', onButtonOver)
+        .on('pointerout', onButtonOut);
+
+    // Use mouse-only events
+    // .on('mousedown', onButtonDown)
+    // .on('mouseup', onButtonUp)
+    // .on('mouseupoutside', onButtonUp)
+    // .on('mouseover', onButtonOver)
+    // .on('mouseout', onButtonOut)
+
+    // Use touch-only events
+    // .on('touchstart', onButtonDown)
+    // .on('touchend', onButtonUp)
+    // .on('touchendoutside', onButtonUp)
+
+    // add it to the stage
+    app.stage.addChild(butt);
+
+    // add button to array
+    buttons.push(butt);
+}
 
 
 function onButtonDown() {
 
+
     this.isdown = true;
-    this.texture = textureButtonDown;
+    this.texture = this.down;
     this.alpha = 1;
+    sound.play();
 }
 
 function onButtonUp() {
     this.isdown = false;
     if (this.isOver) {
-        this.texture = textureButtonOver;
+        this.texture = this.over;
     }
     else {
-        this.texture = textureButton;
+        this.texture = this.normal;
     }
 }
 
@@ -127,7 +170,7 @@ function onButtonOver() {
     if (this.isdown) {
         return;
     }
-    this.texture = textureButtonOver;
+    this.texture = this.over;
 }
 
 function onButtonOut() {
@@ -135,18 +178,38 @@ function onButtonOut() {
     if (this.isdown) {
         return;
     }
-    this.texture = textureButton;
+    this.texture = this.normal;
 }
 
 
-bets.addChild(betbg,buttonPlus);
-bets.scale.x = bets.scale.y = (0);
+
+constr(button);
+constr(buttonPlus);
+constr(buttonMinus);
+
+buttons[0].x = 185;
+buttons[0].y = 115;
+
+buttons[1].x = 55;
+buttons[1].y = 55;
+
+buttons[2].x = 345;
+buttons[2].y = 55;
+
+
+
+coins.addChild(coinbg);
+coins.x = 665;
+coins.y = 60;
+
+bets.addChild(betbg,buttonPlus,buttonMinus);
+bets.scale.x = bets.scale.y = (1);
 bets.anchor = (.5);
-bets.x = 155;
-bets.y = 55;
+bets.x = 250;
+bets.y = 60;
 
 
 app.stage.addChild(container);
-container.addChild(background,button, bets);
-container.scale.x = container.scale.y = 0.5;
+container.addChild(background,button, bets,coins );
+container.scale.x = container.scale.y = .5;
 container.y = 40;
